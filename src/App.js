9f37706cb70f22e51c5c14294814a3704b79107a
wonhyejong <whj0524@naver.com>
2022-07-10 23:29:04 +0900

    create route

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Main from './pages/Main';
 import Company from './pages/Company';
+import {Routes ,Route} from 'react-router-dom' ;
 function App() {
   return (
     <div className="App">
-        <Main />
-        <Company />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/company' element={<Company />} /> 
+      </Routes>
     </div>
   );
 }
